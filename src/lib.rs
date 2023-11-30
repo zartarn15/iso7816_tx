b9589edb767244e0ee6defb1e7597cd0e3d294a5
Oleksandr Kravchenko <zartarn15@gmail.com>
2023-11-30 09:29:43 +0200

    Update example in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -8,20 +8,73 @@
 //!
 //! # Examples
 //! ```no_run
-//! use iso7816_tx::TransmissionBuilder;
+//!fn main() {
+//!    use iso7816_tx::TransmissionBuilder;
 //!
-//! let mut buf = [0u8; 258];
-//! let mut t = TransmissionBuilder::<(), ()>::new()
-//!     .set_read_cb(|_, _| Ok(0))
-//!     .set_write_cb(|_, _| Ok(0))
-//!     .set_sleep_cb(|_| ())
-//!     .set_nad(1, 2)
-//!     .build();
+//!    let mut buf = [0u8; 258];
+//!    let mut t = TransmissionBuilder::new()
+//!        .set_init_cb(open)
+//!        .set_release_cb(close)
+//!        .set_reset_cb(reset)
+//!        .set_read_cb(read)
+//!        .set_write_cb(write)
+//!        .set_sleep_cb(sleep)
+//!        .set_nad(15, 51)
+//!        .build();
 //!
-//! let atr = t.atr().expect("Failed to get ATR");
+//!    let atr = t.atr().expect("Failed to get ATR");
 //!
-//! let capdu = &[0x80, 0xca, 0x9f, 0x7f];
-//! let rapdu = t.transmit(capdu, &mut buf).expect("Failed to transmit");
+//!    let capdu = &[0x80, 0xca, 0x9f, 0x7f];
+//!    let rapdu = t.transmit(capdu, &mut buf).expect("Failed to transmit");
+//!}
+//!
+//!fn open() -> Result<Option<Interface>, Error> {
+//!    // Initialize connection interface
+//!    // ...
+//!
+//!    Ok(Some(Interface::default()))
+//!}
+//!
+//!fn close(interface: Option<&Interface>) -> Result<Option<Interface>, Error> {
+//!    // Release connection interface
+//!    // ...
+//!
+//!    Ok(None)
+//!}
+//!
+//!fn reset(interface: Option<&Interface>) -> Result<(), Error> {
+//!    // Cold reset implementation
+//!    // ...
+//!
+//!    Ok(())
+//!}
+//!
+//!fn read(interface: Option<&Interface>, buf: &mut [u8]) -> Result<usize, Error> {
+//!    // Read data from connection interface
+//!    // ...
+//!
+//!    Ok(0)
+//!}
+//!
+//!fn write(interface: Option<&Interface>, buf: &[u8]) -> Result<usize, Error> {
+//!    // Write data to connection interface
+//!    // ...
+//!
+//!    Ok(0)
+//!}
+//!
+//!fn sleep(ms: u32) {
+//!    // Sleep implementation
+//!    // ...
+//!}
+//!
+//!// Connection interface context
+//!#[derive(Default)]
+//!struct Interface{}
+//!
+//!// Interface errors
+//!#[derive(Debug)]
+//!enum Error {}
 //!
 //! ```
 
